526ac2fd179d4251f623146d0981b13818c8caf5
Endre Moen <emoen@users.noreply.github.com>
2022-09-23 11:37:40 +0200

    Update logistic_logit.r

--- scripts/logistic_logit.r
+++ scripts/logistic_logit.r
@@ -1,6 +1,7 @@
 aseq = seq(-10, 10, by=0.1)
-logistic = function(a=0, b=1, x=seq(0,1,by=0.1)) { return ( exp(a+b*x)/(1+exp(a+b*x)) ) }
-logit = function(a=0, b=1, x=seq(0,1,by=0.1)) { return ( exp(a+b*x)/(1+exp(a+b*x)) ) }
+logistic = function(a=0, b=1, x=aseq) { return ( exp(a+b*x)/(1+exp(a+b*x)) ) }
+aseq = seq(0, 1, by=0.01)
+logit = function(x=aseq) { return ( log(x/(1-x)) ) }
 
 
 plot(aseq, logistic(0, 1, aseq))
